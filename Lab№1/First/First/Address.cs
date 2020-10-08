dfdc848f0eb0d961e191332a73672c3efff9124f
OooODEV <igyana345@gmail.com>
2020-10-08 10:58:15 +0300

    First commit

---
+++ Lab№1/First/First/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First
+{
+    class Address
+    {
+        public int index { get; set; }
+        public int house { get; set; }
+        public int apartament { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+
+        public void GetInfo(int Index, int House, int Apartament, string Country, string City)
+        {
+            Console.WriteLine("Index: {0} \nHouse: {1} \nApartament: {2} \nCountry: {3} \nCity: {4}", Index, House, Apartament, Country, City);
+        }
+    }
+}
---
+++ Lab№1/First/First/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address addressInfo = new Address();
+
+            addressInfo.index = 5;
+            addressInfo.house = 2;
+            addressInfo.apartament = 43;
+            addressInfo.country = "Ukraine";
+            addressInfo.city = "Kyiv";
+
+            addressInfo.GetInfo(addressInfo.index, addressInfo.house, addressInfo.apartament, addressInfo.country, addressInfo.city);
+        }
+    }
+}
